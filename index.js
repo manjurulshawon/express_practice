29aa9470a668953cb230a03dcc9def595ba16b39
manjurulshawon <shawon3154@.com>
2023-09-05 16:13:02 +0600

    updated

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const express = require("express");
 
 app= express();
 app.get('/',(req,res)=>{
-    res.send("Hello From Express")
-})
+    res.send("Hello From Home page")
+});
+
+app.get('/about',(req,res)=>{
+    res.send("Hello From about page")
+});
+
+app.get('/contract',(req,res)=>{
+    res.send("Hello From contract page")
+});
+
+
 
 app.listen(8000,()=>{
     console.log("Server Running.....")
